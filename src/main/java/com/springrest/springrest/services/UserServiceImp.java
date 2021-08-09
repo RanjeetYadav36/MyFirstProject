7bae79b37882419ca4c89ecc07b6868d9f0cd0e9
RanjeetYadav36 <ranjeet.yadav@innobitsystems.com>
2021-08-09 16:35:48 +0530

    add sample project

---
+++ src/main/java/com/springrest/springrest/services/UserServiceImp.java
@@ -0,0 +1,43 @@
+package com.springrest.springrest.services;
+
+import java.util.*;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springrest.springrest.Dao.UserDao;
+import com.springrest.springrest.entities.User;
+
+@Service
+public class UserServiceImp implements UserService {
+@Autowired
+private UserDao userdao;
+	//List<User> list;
+	
+	public UserServiceImp() {
+		// TODO Auto-generated constructor stub
+		/*list = new ArrayList<>();
+		list.add(new User(1,"ram",12345));
+		list.add(new User(2,"mohan",466897));
+		list.add(new User(3,"saim",56896));
+		*/
+		}
+	@Override
+	public List<User> getUser() {
+		// TODO Auto-generated method stub
+		return userdao.findAll();
+	}
+	public User addUser(User user) {
+		
+		//list.add(user);
+		//return user;
+		 userdao.save(user);
+		return user;
+	}
+	public User UpdateUser(User user) {
+		userdao.save(user);
+		return user;
+	}
+
+}
